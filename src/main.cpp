45974b4be38f3f707d396050a46c2998490ef10b
Niko Kivel <niko.kivel@psi.ch>
2020-10-12 00:08:04 +0200

    Ir2ctrl (#2)
    * frame work for IR in Ctrl
    
    * IR receiver handled in control class

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,16 @@
 #include "main.h"
 
-#include <SPI.h>
-
 LCDisplay myDisplay;
 AuxBoard myAuxBoard;
 IRremoteDecoder myIR;
 Volume myVolume(&myDisplay, &myAuxBoard);
-Control Crtl(&myDisplay, &myVolume);
+Control Crtl(&myDisplay, &myVolume, &myIR);
+// Control myControls(&myDisplay, &myVolume, &myIR);
 
 uint8_t lastSPIVolume=0;
 
 void setup() {
+
   arduinoInit();
   /* Start the IR-receiver */
   myIR.initIR();
@@ -35,28 +35,9 @@ void setup() {
 int n=0;
 int i=0;
 void loop() {
-  // IR test
-
-  myIR.getIRcode();
-  if(myIR.valid){
-    if(myIR.ircode == myIR.irTable.Up)    Crtl.vol->up();
-    if(myIR.ircode == myIR.irTable.Down)  Crtl.vol->down();
-    if(myIR.ircode == myIR.irTable.Mute)  Crtl.vol->mute();
-    #if defined(APPLE)
-      if(myIR.ircode == myIR.irTable.Menu)  Crtl.vol->mute();
-    #endif
-    if(myIR.ircode == myIR.irTable.On)    Crtl.buttons.Power=true;
-    if(myIR.ircode == myIR.irTable.Enter) Crtl.buttons.Enter=true;
-    if(myIR.ircode == myIR.irTable.Inp1) i=1;
-    if(myIR.ircode == myIR.irTable.Inp2) i=2;
-    if(myIR.ircode == myIR.irTable.Inp3) i=3;
-    if(myIR.ircode == myIR.irTable.Inp4) i=4;
-    if(myIR.ircode == myIR.irTable.Inp5) i=5;
-    if(myIR.ircode == myIR.irTable.Inp6) i=6;
-
-  };
 
   Crtl.checkButtons();
+  Crtl.checkIR();
 
   if(Crtl.factoryReset){
     // resetFactoryDefaults();
